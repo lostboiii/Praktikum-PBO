fdef2a4afe264aaef468313a5ec4a678f8251115
lostboiii <dafasetiadi55@gmail.com>
2024-10-18 09:50:17 +0700

    UTS

--- UTS/UTS1.java
+++ UTS/UTS1.java
@@ -16,6 +16,8 @@ public class UTS1 {
     public static void main(String[] args) {
         Komputer komputer = new Komputer("Asus", 2000, 1024, "Intel Core i5");
         komputer.tampil();
+        Pc pc  = new Pc(komputer.merk, komputer.kecProsesor, komputer.sizeMemory, komputer.jnsProcessor, 15);
+        pc.tampilPc();
         Leptop leptop = new Leptop("Li-ion", komputer.merk, komputer.kecProsesor, komputer.sizeMemory, komputer.jnsProcessor);
         leptop.tampilLeptop();
         Mac mac = new Mac("Li-ion", komputer.merk, komputer.kecProsesor, komputer.sizeMemory, komputer.jnsProcessor, "Security aman coy");
